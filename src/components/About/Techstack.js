1783ad4dad1274e2e853cf4b833e49a32b0df768
Javier.Vivas <79529756+Bujio@users.noreply.github.com>
2024-01-28 17:11:04 +0100

    add icon

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -7,39 +7,64 @@ import {
   DiNodejs,
   DiMongodb,
   DiGit,
+  DiCss3,
+  DiHtml5,
+  DiDatabase,
 } from "react-icons/di";
 import {
   SiBootstrap,
   SiGoogleanalytics,
+  SiGulp,
   SiPostgresql,
+  SiPostman,
+  SiSap,
+  SiSass,
+  SiSqlite,
 } from "react-icons/si";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
+        <DiReact />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
+        <DiNodejs />
       </Col>
 
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
       </Col>
-
       <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
+        <DiDatabase />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostgresql />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <SiBootstrap />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSass />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGulp />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiGoogleanalytics />
